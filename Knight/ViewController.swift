88dddb50f9a02fff28c1772253bce8041ded8811
Zhijun Sheng <zhijunsheng@gmail.com>
2019-06-19 01:32:51 -0400

    created the empty xcode proj

---
+++ Knight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knight
+//
+//  Created by Donald Sheng on 2019-06-19.
+//  Copyright © 2019 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
