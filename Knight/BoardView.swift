51ea3b9d2ed3dd571f5bdaf390c01e5a31544664
Zhijun Sheng <zhijunsheng@gmail.com>
2019-06-19 01:39:27 -0400

    added a view for game board

---
+++ Knight/BoardView.swift
@@ -0,0 +1,17 @@
+//
+//  BoardView.swift
+//  Knight
+//
+//  Created by Donald Sheng on 2019-06-19.
+//  Copyright © 2019 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
