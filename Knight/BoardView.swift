6dc1e8c0531ae226cbc571e4662cf242ef582401
Zhijun Sheng <zhijunsheng@gmail.com>
2019-06-19 02:07:56 -0400

    drew game board

--- Knight/BoardView.swift
+++ Knight/BoardView.swift
@@ -9,9 +9,49 @@
 import UIKit
 
 class BoardView: UIView {
+    
+    let cols: Int = 9
+    let rows: Int = 10
+    
+    let originX: CGFloat = 27
+    let originY: CGFloat = 43
+    let cellSide: CGFloat = 29
 
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        drawBoard()
     }
 
+    private func drawBoard() {
+        drawGrid()
+    }
+    
+    private func drawGrid() {
+        let path = UIBezierPath()
+        for i in 0..<rows {
+            path.move(to: CGPoint(x: originX, y: originY + CGFloat(i) * cellSide))
+            path.addLine(to: CGPoint(x: originX + CGFloat(cols - 1) * cellSide, y: originY + CGFloat(i) * cellSide))
+        }
+        for i in 0..<cols {
+            path.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY))
+            path.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + 4 * cellSide))
+            
+            path.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + 5 * cellSide))
+            path.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(rows - 1) * cellSide))
+        }
+        
+        for i in 0..<2 {
+            path.move(to: CGPoint(x: originX + CGFloat(i * 8) * cellSide, y: originY + 4 * cellSide))
+            path.addLine(to: CGPoint(x: originX + CGFloat(i * 8) * cellSide, y: originY + 5 * cellSide))
+            
+            // palace
+            
+            path.move(to: CGPoint(x: originX + 3 * cellSide, y: originY + CGFloat(i * 7) * cellSide))
+            path.addLine(to: CGPoint(x: originX + 5 * cellSide, y: originY + CGFloat(i * 7 + 2) * cellSide))
+            
+            path.move(to: CGPoint(x: originX + 5 * cellSide, y: originY + CGFloat(i * 7) * cellSide))
+            path.addLine(to: CGPoint(x: originX + 3 * cellSide, y: originY + CGFloat(i * 7 + 2) * cellSide))
+        }
+        
+        path.stroke()
+    }
 }
